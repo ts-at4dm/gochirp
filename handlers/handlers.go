09a1dc8c150960464e7f3543a9d44465c4ccf1a3
ts-at4dm <terran.stone@maine.edu>
2025-03-28 23:38:34 -0400

    Organized code and added messaging
    organized the code into packages, implemented connection handling to accept usernames and store messages in a global map with thread safety, and enabled message processing by logging and echoing received messages back to clients.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,13 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
+	"GoChirp/models"
 )
+
+// Global map to store messages
+var UserMessagesMap = make(map[string]*models.UserMessages)
+var mu sync.Mutex // Mutex to handle concurrent access
 
 func HandleConnections(conn net.Conn) {
 	defer conn.Close()
 
 	// Read data from connection
 	buffer := make([]byte, 5*1024) // 5kb buffer
+	var username string
+
+	// First message should be the username
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Printf("Read error: %v", err)
+		return
+	}
+	username = string(buffer[:n])
+
+	// Lock mutex, initialize UserMessages for the username, then unlock.
+	mu.Lock()
+	userMessagesMap[username] = &models.UserMessages{Username: username, Message: []string{}}
+	mu.Unlock()
+
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
@@ -22,6 +43,10 @@ func HandleConnections(conn net.Conn) {
 		msg := string(buffer[:n])
 		fmt.Printf("Recieved: %s", msg)
 
+		mu.Lock()
+		userMessagesMap[usernam].Message = append(userMessagesMap[username].Message, msg)
+		mu.Unlock()
+		
 		// Echo back to the client
 		_, err = conn.Write([]byte("Echo:" + msg))
 		if err != nil {
